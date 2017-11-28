a63ffdc6108baaeac9e98b91788acb720ad8502d
max <mkuchin@gmail.com>
2017-11-28 20:42:27 +1100

    userId

--- generate.groovy
+++ generate.groovy
@@ -4,12 +4,12 @@ import groovy.transform.Field
 @Field final expiry = 300 // 5 min
 @Field final secret = ' secret'
 
-String encodeUri(String uri) {
+String encodeUri(String uri, int userId) {
   int unixTime =  new Date().time/1000
   int expiryTime = unixTime + expiry
-  String v = "$expiryTime$uri$secret"
+  String v = "$expiryTime$uri$userId$secret"
   String md5 = MessageDigest.getInstance("MD5").digest(v.bytes).encodeBase64().toString().tr('+/', '-_').replace('=', '')
-  return "${uri}?t=$expiryTime&h=$md5"
+  return "${uri}?t=$expiryTime&u=$userId&h=$md5"
 }
 
-println encodeUri('/s/')
+println encodeUri('/s/', 5)
